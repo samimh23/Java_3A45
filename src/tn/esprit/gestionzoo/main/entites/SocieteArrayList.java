3106b9bf21cd1466c2d9a61e105fc9c655810b9b
samimh23 <sami.mahjoub@esprit.tn>
2023-11-19 23:26:08 +0100

    adding

---
+++ src/tn/esprit/gestionzoo/main/entites/SocieteArrayList.java
@@ -0,0 +1,2 @@
+package tn.esprit.gestionzoo.main.entites;public class SocieteArrayList {
+}
--- src/tn/esprit/gestionzoo/main/main/Main.java
+++ src/tn/esprit/gestionzoo/main/main/Main.java
@@ -58,12 +58,10 @@ public class Main {
         aquatic.eatMeat(Food.MEAT);
 
 
-        // Create a Penguin object
         Penguin penguin12 = new Penguin("Pen", "dhd", 3, true, "tunis", 17.5f);
         penguin12.eatMeat(Food.MEAT);
         penguin12.swim();
 
-        // Create a Terrestrial object
         Terrestrial terrestrial12 = new Terrestrial("Yekel", "hmed", 5, true, 10);
         terrestrial12.eatMeat(Food.MEAT);
         terrestrial12.eatPlantAndMeet(Food.BOTH);
